94cf4f09b75b46f5c0f6d3aac149b9a4e2cb9890
ManalAbduqawi <denever_2008@windowslive.com>
2025-09-15 13:47:17 +0100

    create comment model

--- art/models.py
+++ art/models.py
@@ -10,11 +10,31 @@ class Artwork(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     artist = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts")
+    User, on_delete=models.CASCADE, related_name="artworks")
     description = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     art_image = CloudinaryField('image')
     size = models.IntegerField(choices=SIZE, default=0)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"{self.title} | added by {self.artist}" 
+
+
+class Comment(models.Model):
+    """
+    Stores a single comment entry related to :model:`auth.User`
+    and :model:`blog.Post`.
+    """
+    artwork = models.ForeignKey('Artwork',on_delete=models.CASCADE,related_name="comments")
+    artist = models.ForeignKey(User, on_delete=models.CASCADE,related_name="commenter")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"Comment {self.body} by {self.artist}"    
+
 
 
 
