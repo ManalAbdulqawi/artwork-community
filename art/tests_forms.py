66d05c70c335711baf8a28f90a4cebdec81f5fbe
ManalAbduqawi <denever_2008@windowslive.com>
2025-10-09 02:16:18 +0100

    add automated test section in read me file and style bug fixing

--- art/tests_forms.py
+++ art/tests_forms.py
@@ -1,12 +1,10 @@
 from django.test import TestCase
 from .forms import CommentForm
-from .forms import ArtworkForm
-
 
 class TestCommentForm(TestCase):
 
     def test_form_is_valid(self):
-        comment_form = CommentForm({'body': 'This is a great post'})
+        comment_form = CommentForm({'body': 'This is a great artwork'})
         self.assertTrue(comment_form.is_valid())
 
     def test_form_is_invalid(self):
